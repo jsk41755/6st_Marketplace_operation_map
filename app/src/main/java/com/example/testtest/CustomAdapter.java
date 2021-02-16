3fd20fd30763d32edef273acf79f029b410153bb
JSG <jsk41755@naver.com>
2021-02-16 21:46:49 +0900

    ver.0.6.0 (승규 : 검색 필터링, 윤성 : 사진 드래그 및 db 클릭 성공 - 재업)

--- app/src/main/java/com/example/testtest/CustomAdapter.java
+++ app/src/main/java/com/example/testtest/CustomAdapter.java
@@ -24,50 +24,80 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CustomAdapter extends FirebaseRecyclerAdapter<User,CustomAdapter.myviewholder>
+public class CustomAdapter extends FirebaseRecyclerAdapter<User, CustomAdapter.myviewholder>
 {
     public CustomAdapter(@NonNull FirebaseRecyclerOptions<User> options) {
         super(options);
     }
 
-    @Override
-    protected void onBindViewHolder(@NonNull myviewholder holder, int position, @NonNull User model) {
-        holder.id.setText(model.getId());
-        holder.adress.setText(model.getStore());
-        holder.floor.setText(model.getStname());
-        holder.stname.setText(model.getAdress());
-        holder.store.setText(model.getFloor());
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Toast.makeText(context, myMovieDataList.getMovieName(), Toast.LENGTH_SHORT).show();
-            }
-        });
+    public interface OnPersonItemClickListener {               //클릭 이벤트를 어댑터 내에서가 아닌 다른 액티비티에서 처리하고 싶어서 인터페이스 만듦
+        public void onItemClick(View view, int position);
+    }
+
+    private OnPersonItemClickListener mlistener;
+
+    @Override
+    protected void onBindViewHolder(@NonNull myviewholder holder, int position, @NonNull User model) {  //User 클래스 파일과 연결시켜주는 함수
+        holder.id.setText(model.getId());
+        holder.address.setText(model.getAddress());
+        holder.floor.setText(model.getFloor());
+        holder.st_name.setText(model.getSt_name());
+        holder.st_type.setText(model.getSt_type());
+
+        // holder.itemView.setOnClickListener(new View.OnClickListener() {  //카드뷰 눌렀을 때, 우선 로그캣으로 확인 가능.
+        //      @Override
+        //     public void onClick(View v) {
+        //Toast.makeText(context, myMovieDataList.getSt_name(), Toast.LENGTH_SHORT).show();
+        //       }
+        //   });
+
     }
 
 
     @NonNull
     @Override
-    public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
-    {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
+    public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
         return new myviewholder(view);
     }
 
 
-    class myviewholder extends RecyclerView.ViewHolder
+    public void setOnItemclicklistener(OnPersonItemClickListener listener) {
+        this.mlistener = listener;
+    }
+
+
+    class myviewholder extends RecyclerView.ViewHolder    //list_item.xml과 연동시켜주는 함수
     {
         //CircleImageView img;
-        TextView id,adress,floor,stname,store;
-        public myviewholder(@NonNull View itemView)
-        {
+        TextView id, address, floor, st_name, st_type;
+
+        public myviewholder(@NonNull View itemView) {
             super(itemView);
-            id=(TextView)itemView.findViewById(R.id.tv_id);
-            adress=(TextView)itemView.findViewById(R.id.tv_adress);
-            floor=(TextView)itemView.findViewById(R.id.tv_floor);
-            stname=(TextView)itemView.findViewById(R.id.tv_stname);
-            store=(TextView)itemView.findViewById(R.id.tv_store);
+            id = (TextView) itemView.findViewById(R.id.tv_id);
+            address = (TextView) itemView.findViewById(R.id.tv_address);
+            floor = (TextView) itemView.findViewById(R.id.tv_floor);
+            st_name = (TextView) itemView.findViewById(R.id.tv_st_name);
+            st_type = (TextView) itemView.findViewById(R.id.tv_st_type);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {                    //main 액티비티에 클릭 리스너를 하기 위해 여기에 선언. 이유는 토스트는
+                    int position = getAdapterPosition();            //context가 필요하기도 하고, 작업하기도 편해질 듯 해서
+                    if (position != RecyclerView.NO_POSITION ) {
+                        mlistener.onItemClick(view, position);
+
+                    }
+
+
+                }
+            });
+
+
         }
+
+
     }
+
+
 }
--- app/src/main/java/com/example/testtest/MainActivity.java
+++ app/src/main/java/com/example/testtest/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -15,6 +16,9 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SearchView;
 
 import android.view.Menu;
@@ -42,8 +46,12 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     public Button btn_Zone1;
     public Button btn_Zone2;
-
+    ImageView imageview2;           //지도 아이디 값 저장
     ActionBar actionBar;
+
+    private RadioGroup rg;
+    private RadioButton rb_stname, rb_address;
+    private String str_result;
 
     RecyclerView recview;
     CustomAdapter adapter;
@@ -56,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference databaseReference;
 
+    //ItemTouchHelper helper;
 
     //private List<ExampleItem> exampleList;
     //private List<ExampleItem> exampleListFull;
@@ -69,25 +78,108 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("건물명을 입력하시오.");
 
-        actionBar = getSupportActionBar();
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFED3B00")));      //액션바 색상
 
-        if(Build.VERSION.SDK_INT>=21){                                                                     //상태바 색상
-            window=this.getWindow();
-            window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
+
+        actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFA4614")));      //액션바 색상
+
+        //ActionBar actionBar = getSupportActionBar();
+        // actionBar.hide();
+        str_result = getString(R.string.st_name);
+
+        rg = findViewById(R.id.rg);
+        rb_stname = findViewById(R.id.rb_stname);
+        rb_address = findViewById(R.id.rb_address);
+
+        rb_stname.setChecked(true);
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() { //라디오의 상태 값의 변경됨을 감지.
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                if(checkedId == R.id.rb_stname){
+                    Toast.makeText(MainActivity.this,"남자 라디오 버튼",Toast.LENGTH_SHORT).show();
+                    str_result = getString(R.string.st_name);
+                } else if(checkedId == R.id.rb_address){
+                    Toast.makeText(MainActivity.this,"여자 라디오 버튼",Toast.LENGTH_SHORT).show();
+                    str_result = getString(R.string.address);
+                }
+            }
+        });
+
+        if (Build.VERSION.SDK_INT >= 21) {                                                                     //상태바 색상
+            window = this.getWindow();
+            window.setStatusBarColor(this.getResources().getColor(R.color.firefighter_color));
         }
 
-        recview=(RecyclerView)findViewById(R.id.recyclerView);                                            //DB 리싸이클러뷰
+        recview = (RecyclerView) findViewById(R.id.recyclerView);                                            //DB 리싸이클러뷰
         recview.setLayoutManager(new LinearLayoutManager(this));
+        imageview2 = (ImageView) findViewById(R.id.imageView2);
 
-        FirebaseRecyclerOptions<User> options =
+        FirebaseRecyclerOptions<User> options =                                                     //검색 알고리즘_1
                 new FirebaseRecyclerOptions.Builder<User>()
                         .setQuery(FirebaseDatabase.getInstance().getReference().child("User"), User.class)
                         .build();
 
-        adapter=new CustomAdapter(options);
+        adapter = new CustomAdapter(options);
         recview.setAdapter(adapter);
+
+        //    helper = new ItemTouchHelper(new ItemTouchHelperCallback(adapter));
+        //    helper.attachToRecyclerView(recview);                                                    //recyclerview에  itemtouchhelper 붙임 스와이프
+
+        imageview2.setOnTouchListener(new View.OnTouchListener() {                  //지도 터치로 이동시키기. 아직 완성 x
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int parentWidth = ((ViewGroup) view.getParent()).getWidth();
+                int parentHeight = ((ViewGroup) view.getParent()).getHeight();    //각각 부모view의 너비와 높이
+
+                int action = motionEvent.getAction();
+
+
+                if (action == MotionEvent.ACTION_DOWN) {  //뷰를 눌렀을 때
+                    float curX = motionEvent.getX();
+                    float curY = motionEvent.getY();
+
+                    Log.d("viewTest", "oldXvalue : " + curX + " oldYvalue : " + curY);    // View 내부에서 터치한 지점의 상대 좌표값.
+                    Log.d("viewTest", "v.getX() : " + view.getX());    // View 의 좌측 상단이 되는 지점의 절대 좌표값.
+                    Log.d("viewTest", "RawX : " + motionEvent.getRawX() + " RawY : " + motionEvent.getRawY());    // View 를 터치한 지점의 절대 좌표값.
+                    Log.d("viewTest", "v.getHeight : " + view.getHeight() + " v.getWidth : " + view.getWidth());    // View 의 Width, Height
+
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+                    // 뷰 이동 중
+                    view.setX(view.getX() + (motionEvent.getX()) - (view.getWidth() / 2));
+                    view.setY(view.getY() + (motionEvent.getY()) - (view.getHeight() / 2));
+
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    // 뷰에서 손을 뗌
+
+                    if (view.getX() < 0) {
+                        view.setX(0);
+                    } else if ((view.getX() + view.getWidth()) > parentWidth) {
+                        view.setX(parentWidth - view.getWidth());
+                    }
+
+                    if (view.getY() < 0) {
+                        view.setY(0);
+                    } else if ((view.getY() + view.getHeight()) > parentHeight) {
+                        view.setY(parentHeight - view.getHeight());
+                    }
+                }
+                return true;
             }
+        });
+
+        adapter.setOnItemclicklistener(new CustomAdapter.OnPersonItemClickListener() {    //카드뷰 클릭 시 작동.
+            @Override
+            public void onItemClick(View view, int position) {
+                User user = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), user.getId()+"가 선택됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
 
     @Override
     protected void onStart() {
@@ -103,7 +195,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {                                                 //검색옵션 기능
+    public boolean onCreateOptionsMenu(Menu menu) {                                                 ////검색 알고리즘_2
         getMenuInflater().inflate(R.menu.example_menu, menu);
 
         MenuItem item = menu.findItem(R.id.search);
@@ -124,20 +216,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         return super.onCreateOptionsMenu(menu);
     }
-        private void processsearch(String s)                                                        //검색 기능 구현
-        {
-            FirebaseRecyclerOptions<User> options =
-                    new FirebaseRecyclerOptions.Builder<User>()
-                            .setQuery(FirebaseDatabase.getInstance().getReference().child("User").orderByChild("stname").startAt(s).endAt(s+"\uf8ff"), User.class)
-                            .build();
 
-            adapter=new CustomAdapter(options);
-            adapter.startListening();
-            recview.setAdapter(adapter);
+    private void processsearch(String s)                                                        //검색 알고리즘_3
+    {
+        FirebaseRecyclerOptions<User> options =
+                new FirebaseRecyclerOptions.Builder<User>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("User").orderByChild(str_result).startAt(s).endAt(s + "\uf8ff"), User.class)
+                        .build();
 
-        }
+        adapter = new CustomAdapter(options);
+        adapter.startListening();
+        recview.setAdapter(adapter);
+        adapter.setOnItemclicklistener(new CustomAdapter.OnPersonItemClickListener() {    //카드뷰 클릭 시 작동.
+            @Override
+            public void onItemClick(View view, int position) {
+                User user = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), user.getId()+"가 선택됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
 
     public void Zone1() {
         Intent intent = new Intent(this, Zone1.class);
